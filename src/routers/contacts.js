7fcabb53c106affe68020db0a95a61bcfa99abc9
Didkovskyi Davyd <116342774+Daedraglob01@users.noreply.github.com>
2024-11-15 12:48:09 +0200

    hw

---
+++ src/routers/contacts.js
@@ -0,0 +1,42 @@
+import { Router } from 'express';
+
+import {
+  createContactController,
+  deleteContactController,
+  getAllContactsController,
+  getContactByIdController,
+  updateContactController,
+  upsertContactController,
+} from '../controllers/contacts.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
+import { isValidId } from '../middlewares/isValidId.js';
+import { authenticate } from '../middlewares/authenticate.js';
+
+const router = Router();
+router.use(authenticate);
+router.get('/', ctrlWrapper(getAllContactsController));
+router.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController));
+router.post(
+  '/',
+  validateBody(createContactSchema),
+  ctrlWrapper(createContactController),
+);
+router.put(
+  '/:contactId',
+  isValidId,
+  validateBody(createContactSchema),
+  ctrlWrapper(upsertContactController),
+);
+router.patch(
+  '/:contactId',
+  isValidId,
+  validateBody(updateContactSchema),
+  ctrlWrapper(updateContactController),
+);
+router.delete('/:contactId', isValidId, ctrlWrapper(deleteContactController));
+export default router;
---
+++ src/services/auth.js
@@ -0,0 +1,63 @@
+import bcrypt from 'bcrypt';
+import createHttpError from 'http-errors';
+import { UsersCollection } from '../db/models/users.js';
+import { SessionsCollection } from '../db/models/session.js';
+import { createSession } from '../utils/createSession.js';
+
+export const registerUser = async (userData) => {
+  const user = await UsersCollection.findOne({
+    email: userData.email,
+  });
+  if (user) throw createHttpError(409, 'Email in use');
+  const encryptedPassword = await bcrypt.hash(userData.password, 10);
+  return await UsersCollection.create({
+    ...userData,
+    password: encryptedPassword,
+  });
+};
+
+export const loginUser = async (userData) => {
+  const user = await UsersCollection.findOne({ email: userData.email });
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
+  const isEqualPassword = await bcrypt.compare(
+    userData.password,
+    user.password,
+  );
+  if (!isEqualPassword) {
+    throw createHttpError(401, 'Unauthorized! Incorrect password');
+  }
+  await SessionsCollection.deleteOne({ userId: user._id });
+  const session = createSession();
+
+  return await SessionsCollection.create({
+    userId: user._id,
+    ...session,
+  });
+};
+
+export const refreshUsersSession = async ({ sessionId, refreshToken }) => {
+  const session = await SessionsCollection.findOne({
+    _id: sessionId,
+    refreshToken,
+  });
+  if (!session) {
+    throw createHttpError(401, 'Session not found');
+  }
+  const isSessionTokenExpired =
+    new Date() > new Date(session.refreshTokenValidUntil);
+  if (isSessionTokenExpired) {
+    throw createHttpError(401, 'Session token expired');
+  }
+  const newSession = createSession();
+  await SessionsCollection.deleteOne({ _id: sessionId, refreshToken });
+  return await SessionsCollection.create({
+    userId: session.userId,
+    ...newSession,
+  });
+};
+
+export const logoutUser = async (sessionId) => {
+  await SessionsCollection.deleteOne({ _id: sessionId });
+};
